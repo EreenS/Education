34c5667fc8403a7c61ba4ac4b9ca9241cbbca9b6
Berat Eren Sevindi <sevindiberateren@gmail.com>
2026-04-22 14:33:29 +0300

    Add onAddToCart functionality to Course component

--- src/Course.js
+++ src/Course.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-function Course({ title, description }) {
+function Course({ title, description, onAddToCart }) {
     return (
-        <div className="card">
+        <div className="card course-card">
             <div className="card-content">
                 <p className="title is-4">{title}</p>
                 <p className="subtitle is-6">{description}</p>
             </div>
+            <footer className="card-footer">
+                <button className="button is-success is-fullwidth" onClick={onAddToCart}>
+                    Sepete Ekle
+                </button>
+            </footer>
         </div>
     );
 }
